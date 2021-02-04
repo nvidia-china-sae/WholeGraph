2d2fe49ee6e8521671201887fe465c99355bbaec
Jiaxing Qi <jqi@nvidia.com>
2021-02-04 03:04:13 -0500

    clean

--- build/run.sh
+++ build/run.sh
@@ -1,9 +1,4 @@
-#CUDA_VISIBLE_DEVICES=1,2,3,4,5,6,7 PYTHONPATH=.. horovodrun --disable-cache -np 7 -H localhost:7 \
-#	python3 ../examples/torch/papers100m_benchmark.py \
-#	-g '../dataset/ogbn_papers100M/converted' -b 512 -e 200 -w 0  -s 16 -n 16 -u -o 'papers100m' \
-#	--uselabel -r 10 --hiddensize 256 -t 100 --lr 0.01
-
-CUDA_VISIBLE_DEVICES=0,1 PYTHONPATH=.. horovodrun --disable-cache -np 2 -H localhost:2 \
+CUDA_VISIBLE_DEVICES=1,2,3,4,5,6,7 PYTHONPATH=.. horovodrun --disable-cache -np 7 -H localhost:7 \
 	python3 ../examples/torch/papers100m_benchmark.py \
-	-g '../dataset/ogbn_arxiv/converted' -b 512 -e 2 -w 0  -s 16 -n 16 -u -o 'arxiv' \
-	--uselabel -r 2 --hiddensize 256 -t 100 --lr 0.01
+	-g '../dataset/ogbn_papers100M/converted' -b 512 -e 200 -w 0  -s 16 -n 16 -u -o 'papers100m' \
+	--uselabel -r 10 --hiddensize 256 -t 100 --lr 0.01
--- docker.sh
+++ docker.sh
@@ -1,10 +1,7 @@
 docker build -t wholegraph.pytorch -f Dockerfile.pytorch .
 HERE=`pwd`
 docker run --gpus all -it --rm --privileged=true \
-           --ipc=host \
            -v $HERE:$HERE \
-    	   -v /raid/privatedata/pursuit/dongxuy/dataset/gnn/dataset:$HERE/dataset \
            -w $HERE \
-	   -u $(id -u):$(id -g) \
            wholegraph.pytorch
 
