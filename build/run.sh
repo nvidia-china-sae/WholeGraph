62437527bd9e9842ee91075fc063f53836939854
Xiaonan Song <xiaonans@nvidia.com>
2021-02-22 14:13:41 +0800

    add attributions

--- build/run.sh
+++ build/run.sh
@@ -1,3 +1,26 @@
+#
+# The MIT License (MIT)
+# 
+# Copyright (c) 2021, NVIDIA CORPORATION. All rights reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of
+# this software and associated documentation files (the "Software"), to deal in
+# the Software without restriction, including without limitation the rights to
+# use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+# the Software, and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+# FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+# COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+# IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+
 CUDA_VISIBLE_DEVICES=1,2,3,4,5,6,7 PYTHONPATH=.. horovodrun --disable-cache -np 7 -H localhost:7 \
 	python3 ../examples/torch/papers100m_benchmark.py \
 	-g '../dataset/ogbn_papers100M/converted' -b 512 -e 200 -w 0  -s 16 -n 16 -u -o 'papers100m' \
